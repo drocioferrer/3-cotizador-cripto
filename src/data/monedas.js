8ed28edb85136f086ab0f0c97208fc6211c15c1e
drocioferrer <drocioferrer@gmail.com>
2022-08-12 18:43:17 -0300

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar estadounidense'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'ARG', nombre: 'Peso argentino'}
+]
+
+export {
+    monedas
+}
